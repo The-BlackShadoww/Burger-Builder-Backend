2e6c20f558c028a80ae1285cbd3c1736f93eb803
Ashikur Rahman <ashikur.jebu111@gmail.com>
2023-10-04 21:08:59 +0600

    ready to deploy

---
+++ routers/paymentRouter.js
@@ -0,0 +1,113 @@
+const router = require("express").Router();
+const SSLCommerz = require("ssl-commerz-node");
+const PaymentSession = SSLCommerz.PaymentSession;
+const { Payment } = require("../models/payment");
+const { Profile } = require("../models/profile");
+const path = require("path");
+const FormData = require("form-data");
+const fetch = require("node-fetch");
+const authorize = require("../middleware/authorize");
+// const {
+//     initPayment,
+//     ipn,
+//     paymentSuccess,
+// } = require("../controllers/paymentController");
+
+const initPayment = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const profile = await Profile.findOne({ user: userId });
+        const { address1, address2, city, state, postcode, country, phone } =
+            profile;
+
+        const total_amount = 0;
+
+        const total_item = 1;
+
+        const tran_id =
+            "_" +
+            Math.random().toString(36).substr(2, 9) +
+            new Date().getTime();
+
+       
+        const payment = new PaymentSession(
+            true,
+            process.env.SSLCOMMERZ_STORE_ID,
+            process.env.SSLCOMMERZ_STORE_PASSWORD
+        );
+
+        //! Set the urls
+        payment.setUrls({
+            success: "http://localhost:3001/api/payment/success",
+            fail: "yoursite.com/fail",
+            cancel: "yoursite.com/cancel",
+            ipn: "http://localhost:3001/api/payment/ipn",
+        });
+
+        //! Set order details
+        payment.setOrderInfo({
+            total_amount: total_amount,
+            currency: "BDT",
+            tran_id: tran_id,
+            emi_option: 0,
+        });
+
+        //! Set customer info
+        payment.setCusInfo({
+            name: req.user.name,
+            email: req.user.email,
+            add1: address1,
+            add2: address2,
+            city: city,
+            state: state,
+            postcode: postcode,
+            country: country,
+            phone: phone,
+            fax: phone,
+        });
+
+        //! Set shipping info
+        payment.setShippingInfo({
+            method: "Courier",
+            num_item: total_item,
+            name: req.user.name,
+            add1: address1,
+            add2: address2,
+            city: city,
+            state: state,
+            postcode: postcode,
+            country: country,
+        });
+
+        //! Set Product Profile
+        payment.setProductInfo({
+            product_name: "Bohubrihi Burger Builder",
+            product_category: "General",
+            product_profile: "general",
+        });
+
+        response = await payment.paymentInit();
+
+        // let order = new Order({
+        //     cartItems: cartItems,
+        //     user: userId,
+        //     transaction_id: tran_id,
+        //     address: profile,
+        // });
+
+        // if (response.status === "SUCCESS") {
+        //     order.sessionKey = response["sessionkey"];
+        //     await order.save();
+        // }
+
+        return res.status(200).send(response);
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+router.route("/").get(authorize, initPayment);
+// router.route("/ipn").post(ipn);
+// router.route("/success").post(paymentSuccess);
+
+module.exports = router;
